d1be1ca8c36a3a4b523743322fccc77a7527a714
BLooDBRothER <computertechnology446@gmail.com>
2021-07-28 08:21:54 +0530

    Add search suggestion

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ window.addEventListener("mousedown", (e) =>{
     if(setting_cnt.classList.contains("none")) return; 
     setting_cnt.classList.add("none");
 });
+
+//Search Suggestion
+
+const regex = /data=\"([\w ]*)\"/gi;
+url = "https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=master"
+async function fetchSuggestion(){
+    let response = await fetch(url);
+    let result = await response.text();
+    let out = result.matchAll(regex);
+    for(const match of out){
+        console.log(match[1])
+    }
+}
+
+fetchSuggestion();
