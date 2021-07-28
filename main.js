25221d5eec65f9439e772779712b46f0556546c5
BLooDBRothER <computertechnology446@gmail.com>
2021-07-28 13:06:58 +0530

    Add Search api

--- main.js
+++ main.js
@@ -30,10 +30,6 @@ input.addEventListener("mousedown", (e) => {
     border_disp.classList.remove("none");
 });
 
-input.addEventListener("keydown", (e) => {
-    
-});
-
 input.addEventListener("input", (e) => {
     console.log(input.value == '')
     if(input.value == "")
@@ -41,6 +37,7 @@ input.addEventListener("input", (e) => {
         clear.classList.add("none");
         return ;
     }
+    fetchSuggestion(input.value);
     clear.classList.remove("none");
 });
 
@@ -57,14 +54,41 @@ window.addEventListener("mousedown", (e) =>{
 //Search Suggestion
 
 const regex = /data=\"([\w ]*)\"/gi;
-url = "https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=master"
-async function fetchSuggestion(){
+
+async function fetchSuggestion(txt){
+    let url = `https://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=${txt}`;
+    setting_cnt.innerHTML = '';
     let response = await fetch(url);
     let result = await response.text();
     let out = result.matchAll(regex);
+    let li = '';
     for(const match of out){
         console.log(match[1]);
+        li += `<li>
+        <svg class="search-ic" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+        <p>${match[1]}</p>
+        </li>`;
     }
+    li += `<li class="sugg-btn">
+    <button>Google Search</button>
+    <button>I'm Feeling Lucky</button>
+  </li>`
+    sugg_cnt.innerHTML = li;
+    updateLI();
 }
 
-fetchSuggestion();
+function updateLI(){
+    let lis = sugg_cnt.querySelectorAll("li");
+    console.log(lis);
+    lis.forEach(li => {
+        li.addEventListener("mousedown", activateForm);
+    });
+    lis[lis.length - 1].removeEventListener("mousedown", activateForm);
+}
+
+function activateForm(){
+    input.value = this.querySelector("p").innerText;
+    form.submit();
+}
+
+// fetchSuggestion();
